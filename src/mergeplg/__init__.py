fe4be5b867cd31d0dd82d37b716faf666a086c89
Christian Chwala <christian.chwala@kit.edu>
2024-10-15 15:21:43 +0200

    add function to load and transform OpenMRG data, also added test

--- src/mergeplg/__init__.py
+++ src/mergeplg/__init__.py
@@ -7,6 +7,6 @@ from __future__ import annotations
 
 __version__ = "0.0.0"
 
-__all__ = ["__version__", "radolan"]
+__all__ = ["__version__", "io", "radolan"]
 
-from . import radolan
+from . import io, radolan
